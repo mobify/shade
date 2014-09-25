ac6f636108ae602364793f0c1c68e22c32b13ea3
Steve Calvert <scalvert@mobify.com>
2014-09-25 16:53:22 -0700

    Bumping verison number and rebuilding dist

--- dist/shade.js
+++ dist/shade.js
@@ -14,7 +14,7 @@
         Shade.__super__.call(this, element, options, Shade.DEFAULTS);
     }
 
-    Shade.VERSION = '1.0.1';
+    Shade.VERSION = '1.0.2';
 
     Shade.DEFAULTS = {
         cover: document.body,

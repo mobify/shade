095257ac84a898b38977d8aa6d8600ee579d9194
Steve Calvert <scalvert@mobify.com>
2014-10-06 21:40:31 -0700

    Updating version of Velocity

--- dist/shade.js
+++ dist/shade.js
@@ -14,7 +14,7 @@
         Shade.__super__.call(this, element, options, Shade.DEFAULTS);
     }
 
-    Shade.VERSION = '1.0.3';
+    Shade.VERSION = '1.0.4';
 
     Shade.DEFAULTS = {
         cover: document.body,

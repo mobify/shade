f81c3ee164d7e31557f3fba103e7529863a14b0d
Helen <helen@mobify.com>
2014-10-16 08:44:30 -0700

    bump version to 1.1.0

--- dist/shade.js
+++ dist/shade.js
@@ -14,7 +14,7 @@
         Shade.__super__.call(this, element, options, Shade.DEFAULTS);
     }
 
-    Shade.VERSION = '1.0.6';
+    Shade.VERSION = '1.1.0';
 
     Shade.DEFAULTS = {
         cover: document.body,
